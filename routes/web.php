ad8140f22ae1e3c4d5471efa40399d12f54c0e10
AriRahmawati <116719881+AriRahmawati@users.noreply.github.com>
2024-08-22 11:07:21 +0700

    gwd
    dad

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('halamanlogin');
+Route::get('/login', function () {
+    return view('login');
 });
-<<<<<<< Updated upstream
-=======
 Route::get('/register', function () {
     return view('register');
 });
-
->>>>>>> Stashed changes
