c87963e327423fc373c7c3faac13556d7703ec86
AINURRISAALPRIYANI <116731347+AINURRISAALPRIYANI@users.noreply.github.com>
2024-08-22 10:42:05 +0700

    fhh
    hghjgu

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('phone_number', 20);
-            $table->string('picture');
-            $table->enum('role', ['Pelajar', 'Mentor', 'Admin']) -> default('Pelajar');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
